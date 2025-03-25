139620fe3343a774af649185854debb7604cae46
hihihi48 <glmwls9012@gmail.com>
2025-03-25 22:51:55 +0900

    User Mapping까지

---
+++ JSPBook/src/main/java/com/example/jspbook/mapper/UserMapper.java
@@ -0,0 +1,9 @@
+package com.example.jspbook.mapper;
+
+import java.util.List;
+
+import com.example.jspbook.model.User;
+
+public interface UserMapper {
+	List<User> getAllUsers();
+}
---
+++ JSPBook/src/main/java/com/example/jspbook/model/User.java
@@ -0,0 +1,49 @@
+package com.example.jspbook.model;
+
+public class User {
+	private Long userId;
+	private String username;
+	private String password;
+	private String nickname;
+	private String role;
+
+	public Long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getNickname() {
+		return nickname;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+}
